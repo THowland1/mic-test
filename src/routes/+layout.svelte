31a423ceb0bba4333e8da8379673a5c976507578
THowland1 <tomhowland9@gmail.com>
2023-02-06 09:08:14 +0000

    Add Github link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,17 @@
 	import '../app.css';
 </script>
 
-<div class="bg-neutral-800 absolute inset-0 text-neutral-300 overflow-auto p-4">
-	<div class="container m-auto">
+<div
+	class="bg-neutral-800 absolute inset-0 text-neutral-300 overflow-auto p-4 flex flex-col justify-between"
+>
+	<div class="container mx-auto">
 		<slot />
+	</div>
+	<div class="text-center mt-2">
+		<a
+			href="https://github.com/THowland1/mic-test"
+			class=" font-mono text-sm underline underline-offset-4 text-neutral-400 font-light hover:text-neutral-300 "
+			>&lt;source /&gt;</a
+		>
 	</div>
 </div>
