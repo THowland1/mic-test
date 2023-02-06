6429064c57559b50ee771cfc8a7b3afbc4984530
THowland1 <tomhowland9@gmail.com>
2023-02-06 09:34:23 +0000

    Fix og images

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,24 @@
 <script>
 	import '../app.css';
+	import { page } from '$app/stores';
+	const TITLE = 'Mic Check';
+	const DESCRIPTION = "Check all your device's microphones!";
+	const IMAGE = `${$page.url.origin}/og-image.png`;
 </script>
+
+<svelte:head>
+	<title>{TITLE}</title>
+
+	<meta name="description" content={DESCRIPTION} />
+	<meta property="og:title" content={TITLE} />
+	<meta property="og:description" content={DESCRIPTION} />
+	<meta property="og:site_name" content={TITLE} />
+	<meta name="twitter:title" content={TITLE} />
+	<meta name="twitter:description" content={DESCRIPTION} />
+	<meta name="twitter:image:alt" content={TITLE} />
+	<meta property="og:image" content={IMAGE} />
+	<meta name="twitter:image" content={IMAGE} />
+</svelte:head>
 
 <div
 	class="bg-neutral-800 absolute inset-0 text-neutral-300 overflow-auto p-4 flex flex-col justify-between"
